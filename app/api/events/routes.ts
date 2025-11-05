b2db2104438479c1ca836a6da7be2e6bc29047f1
njoroofficial <mosesn579@gmail.com>
2025-11-05 10:23:13 +0300

    Implemented an api get request

--- app/api/events/routes.ts
+++ app/api/events/routes.ts
@@ -1,6 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
 import Event from "@/database/event.model";
 import connectDB from "@/database/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { error } from "console";
 
 export async function POST(req: NextRequest) {
   try {
@@ -21,6 +23,38 @@ export async function POST(req: NextRequest) {
       );
     }
 
+    // get uploaded files
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 }
+      );
+    }
+
+    // convert file into a buffer
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    // pass to cloudinary
+    const uploadedResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "DevEvent",
+          },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadedResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
 
     return NextResponse.json(
@@ -35,6 +69,35 @@ export async function POST(req: NextRequest) {
     return NextResponse.json(
       {
         message: "Event Creation Failed",
+        ...(process.env.NODE_ENV === "development"
+          ? {
+              error: e instanceof Error ? e.message : "unknown",
+            }
+          : {}),
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      {
+        message: "Events fetched successfully",
+        events,
+      },
+      { status: 200 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Event Fetching Failed",
         ...(process.env.NODE_ENV === "development"
           ? {
               error: e instanceof Error ? e.message : "unknown",
